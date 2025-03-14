81e46b748569751bdca361b0cf54d841f076aa0a
j1w0os <102014705+sonjiwoo1215@users.noreply.github.com>
2025-03-14 11:02:22 +0900

    0314

--- controller/BookController.js
+++ controller/BookController.js
@@ -7,7 +7,7 @@ const allBooks = (req, res) => {
 
     let offset = limit * (currentPage - 1);
 
-    let sql = "SELECT * FROM books";
+    let sql = "SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes FROM books";
     let values = [];
 
     if (category_id && news) {
@@ -39,11 +39,19 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    let { id } = req.params;
+    let { user_id } = req.body;
+    let  book_id  = req.params.id;
 
-    let sql = `SELECT * FROM books LEFT JOIN category
-               ON books.category_id = category.id WHERE books.id=?`;
-    conn.query(sql, id,
+    let sql = `SELECT *,
+		            (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+		            (SELECT count(*) FROM likes WHERE user_id=? AND liked_book_id=?) AS liked
+                FROM books
+		        LEFT JOIN category
+		        ON books.category_id = category.category_id
+                WHERE books.id=?;`;
+
+    let values = [user_id, book_id, book_id];
+    conn.query(sql, values,
         (err, results) => {
             if (err) {
                 console.log(err);
